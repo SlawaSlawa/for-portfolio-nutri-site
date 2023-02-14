f46980ed809fec1dbd539f76fc0a0c3bf72d5306
SlawaSlawa <slawapocht4@yandex.ru>
2023-02-14 20:39:46 +0400

    added @media for header & js for burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-console.log('script');
+const burgerBtn = document.querySelector('.burger')
+const navMenu = document.querySelector('.header__nav')
+
+burgerBtn.addEventListener('click', () => {
+    if (burgerBtn.classList.contains('burger--active')) {
+        burgerBtn.classList.remove('burger--active')
+        navMenu.classList.remove('header__nav--active')
+    } else {
+        burgerBtn.classList.add('burger--active')
+        navMenu.classList.add('header__nav--active')
+    }
+})
+
+
+
+// header__nav--active
+// burger--active
 console.log('script2');
 console.log('script3!');
