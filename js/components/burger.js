f09064ab8f9ee12a3a794e6d386a96bef5674005
SlawaSlawa <slawapocht4@yandex.ru>
2023-02-16 21:00:47 +0400

    added @media for hero

---
+++ js/components/burger.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.querySelector('.burger')
+const navMenu = document.querySelector('.header__nav')
+
+burgerBtn.addEventListener('click', () => {
+    if (burgerBtn.classList.contains('burger--active')) {
+        burgerBtn.classList.remove('burger--active')
+        navMenu.classList.remove('header__nav--active')
+    } else {
+        burgerBtn.classList.add('burger--active')
+        navMenu.classList.add('header__nav--active')
+    }
+})
