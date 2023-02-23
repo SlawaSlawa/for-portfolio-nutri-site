5f1a34a3dc847d1015f7c1174b7b593562512702
SlawaSlawa <slawapocht4@yandex.ru>
2023-02-23 21:23:10 +0400

    added @media for section menu

--- js/index.js
+++ js/index.js
@@ -10,11 +10,34 @@ burgerBtn.addEventListener('click', () => {
         navMenu.classList.add('header__nav--active')
     }
 })
-const menuSlider = new Swiper('.swiper', {
+const menuSlider = new Swiper('.menu__slider', {
     spaceBetween: 100,
     navigation: {
         nextEl: '.menu__slider-btn--next',
         prevEl: '.menu__slider-btn--prev',
     },
+    breakpoints: {
+        992: {
+            spaceBetween: 45
+        },
+        1620 : {
+            spaceBetween: 100
+        }
+    }
 });
+
+const menuSliderMin = new Swiper('.menu__slider-min', {
+    spaceBetween: 40,
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.menu__slider-btn--next',
+        prevEl: '.menu__slider-btn--prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2
+        },
+    }
+})
 console.log('script3!');
